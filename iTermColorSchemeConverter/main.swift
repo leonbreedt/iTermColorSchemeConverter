72be43c9076204c8e9680ea9b0b7fc92bdf555e3
Leon Breedt <ljb@bitserf.org>
2014-10-23 20:50:20 +1300

    Initial Commit

---
+++ iTermColorSchemeConverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iTermColorSchemeConverter
+//
+//  Created by Leon Breedt on 23/10/14.
+//  Copyright (c) 2014 Leon Breedt. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
